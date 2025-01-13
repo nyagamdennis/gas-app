cf8dceeba919f793ad39e6c40522c7ed7180e6ae
dennismnyaga <murildennis7@gmail.com>
2025-01-13 15:16:29 +0300

    changed the allowed ho

--- src/getApiUrl.tsx
+++ src/getApiUrl.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
 
 const getApiUrl = () => {
-  const apiUrl = 'http://127.0.0.1:8000';
+  // const apiUrl = 'http://127.0.0.1:8000';
 //  const apiUrl = 'https://murildennis.pythonanywhere.com';
   // const apiUrl = 'https://kibaki.pythonanywhere.com';
+  const apiUrl = 'barakagas.pythonanywhere.com';
   return apiUrl
 }
 
