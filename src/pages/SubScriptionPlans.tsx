90e83bfdcf1384a2a656c55e7d3079c6eab264a1
dennismnyaga <dennisnyaga042@gmail.com>
2025-05-05 18:03:44 +0300

    payment plan added

--- src/pages/SubScriptionPlans.tsx
+++ src/pages/SubScriptionPlans.tsx
@@ -30,7 +30,7 @@ const SubScriptionPlans = () => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <AdminNav
         headerMessage="Manage your subscription plans"
         headerText="Subscription Plans"
